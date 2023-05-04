3ba60caa8d2f562a9765ca28fa8a8b7a67c194c2
Pran-Ker <hebbarpran@gmail.com>
2023-05-04 13:09:49 +0530

    Shrey's part

---
+++ Extra/mysite/grader/migrations/0001_initial.py
@@ -0,0 +1,33 @@
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Essay',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('content', models.TextField(max_length=2000)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Question',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('question_title', models.TextField(max_length=2000)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='essay',
+            name='question',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='grader.Question'),
+        ),
+    ]
